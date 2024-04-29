905d3ed2ebb06de099b72df36184b144f434ec65
jeongbinM <114378753+jeongbinM@users.noreply.github.com>
2024-04-29 14:52:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleMap, LoadScript, } from '@react-google-maps/api';
+import JSZip from 'jszip';
 
 const containerStyle = {
   width: '1400px',
@@ -11,8 +12,8 @@ const center = {
   lng: 127.835941
 };
 
-const MAP_API_KEY = //여기에 지도 api 입력
-const MOUNTAIN_API_KEY = //여기에 등산로 정보요청 api 입력
+const MAP_API_KEY = //지도 api 키 입력
+const MOUNTAIN_API_KEY = //등산로 api 키 입력
 
 function App() {
   const [mountainName, setMountainName] = useState('');
@@ -39,6 +40,15 @@ function App() {
       const data_info = new DOMParser().parseFromString(data, "text/xml");
       console.log("data", data_info);
 
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, "text/xml");
+        
+      // XML에서 ZIP 파일 URL 추출
+      const zipFileURL = xmlDoc.getElementsByTagName('mntnfile')[0].textContent;
+
+      // ZIP 파일 다운로드 및 내부 파일 접근
+      handleZipFile(zipFileURL);
+
       // 받아온 데이터가 없으면 검색 결과가 없음을 알리는 메시지를 표시
       if (data_info.getElementsByTagName('totalCount')[0].textContent === '0') {
         setSearchError(true);
@@ -51,6 +61,31 @@ function App() {
       setSearchError(true); 
     }
   };
+
+  //받아온 ZIP파일 처리 함수
+  const handleZipFile = async (zipFileURL) => {
+    try {
+        // ZIP 파일 다운로드
+        const zipResponse = await fetch(zipFileURL);
+        const zipBlob = await zipResponse.blob();
+
+        // JSZip을 사용하여 ZIP 파일 해제
+        const zip = await JSZip.loadAsync(zipBlob);
+
+        // ZIP 파일 내의 파일 목록 확인
+        zip.forEach((relativePath, zipEntry) => {
+            //파일 이름 출력
+            console.log("File:", relativePath);
+            
+            // 파일 내용
+            zipEntry.async("string").then((content) => {
+                //console.log("Contents:", content);
+            });
+        });
+    } catch (error) {
+        console.error('ZIP 파일을 다운로드하거나 해제하는 중 오류가 발생했습니다:', error);
+    }
+};
 
   return (
     <div className="App">
